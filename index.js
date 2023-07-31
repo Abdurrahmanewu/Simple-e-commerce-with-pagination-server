3800375c5445f09fafe59709bd15bed7bed30478
Abdurrahmanewu <abdurrahman.ewu.bd@gmail.com>
2023-07-31 22:36:32 +0600

    products added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,35 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// connection to MongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jdfs3t2.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const productCollection = client
+      .db("Simple-e-commerce-with-pagination")
+      .collection("products");
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursore = productCollection.find(query);
+      const products = await cursore.toArray();
+      res.send(products);
+    });
+  } finally {
+  }
+}
+run().catch((e) => console.error(e));
 
 // All API
 
